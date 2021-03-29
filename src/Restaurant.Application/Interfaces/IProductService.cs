842c5144f04dbf9f7d7e096663bb685f73b46664
Klayton <odevklayton@gmail.com>
2021-03-29 11:22:00 -0300

    Add product query feature

--- src/Restaurant.Application/Interfaces/IProductService.cs
+++ src/Restaurant.Application/Interfaces/IProductService.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
 using Restaurant.Application.DTOs.Product;
+using Restaurant.Application.QueryParams;
 
 namespace Restaurant.Application.Interfaces
 {
     public interface IProductService
     {
         ProductResponseDTO Insert(ProductPostDTO dto);
+        IEnumerable<ProductResponseDTO> GetAll(ProductQueryParams queryParams);
+        ProductResponseDTO Get(Guid id);
     }
 }
--- src/Restaurant.Application/Services/ProductService.cs
+++ src/Restaurant.Application/Services/ProductService.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using Restaurant.Application.DTOs.Product;
+using Restaurant.Application.Extensions;
 using Restaurant.Application.Interfaces;
+using Restaurant.Application.QueryParams;
 using Restaurant.Core.Entities;
+using Restaurant.Core.Exceptions;
 using Restaurant.Core.Interfaces;
 
 namespace Restaurant.Application.Services
@@ -25,6 +31,33 @@ namespace Restaurant.Application.Services
             _repository.SaveChanges();
 
             return _mapper.Map<ProductResponseDTO>(newEntity);
+        }
+
+        public IEnumerable<ProductResponseDTO> GetAll(ProductQueryParams queryParams)
+        {
+            var query = _repository.GetAll(queryParams.IncludeInactive);
+
+            if (!string.IsNullOrWhiteSpace(queryParams.Description))
+            {
+                query = query.Where(entity =>
+                    entity.Description.ContainsResearch(queryParams.Description));
+            }
+
+            var entities = query.ToList();
+
+            return _mapper.Map<IEnumerable<ProductResponseDTO>>(entities);
+        }
+
+        public ProductResponseDTO Get(Guid id)
+        {
+            var entity = _repository.Get(id);
+
+            if (entity == null)
+            {
+                throw new BusinessException($"{nameof(Department)} not found with {nameof(Department.Id)} '{id}'.");
+            }
+
+            return _mapper.Map<ProductResponseDTO>(entity);
         }
     }
 }
