f5047ae8eaf2360d6bb72f9ca2b46d28e42a296b
Klayton <odevklayton@gmail.com>
2021-04-15 15:17:36 -0300

    Move extensions

--- src/Restaurant.Api/Extensions/ServiceCollectionExtension.cs
+++ src/Restaurant.Api/Extensions/ServiceCollectionExtension.cs
@@ -1,8 +1,14 @@
 using System;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Restaurant.Core.Configurations;
+using Restaurant.Core.Interfaces;
+using Restaurant.Core.Services;
+using Restaurant.Infrastructure.Data;
 using Restaurant.Infrastructure.Filters;
+using Restaurant.Infrastructure.Repositories;
 
 namespace Restaurant.Api.Extensions
 {
@@ -34,6 +40,49 @@ namespace Restaurant.Api.Extensions
         public static void AddMappings(this IServiceCollection services)
         {
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            var openApiInfo = new OpenApiInfo { Title = "Restaurant.Api", Version = "v1" };
+            services.AddSwaggerGen(options => options.SwaggerDoc("v1", openApiInfo));
+        }
+
+        public static void AddContexts(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDataContext>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<IDepartmentService, DepartmentService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IVariationService, VariationService>();
+            services.AddScoped<IMenuService, MenuService>();
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+
+        public static void AddCorsPolicies(this IServiceCollection services)
+        {
+            services.AddCors(ConfigureCorsPolicies);
+        }
+
+        private static void ConfigureCorsPolicies(CorsOptions options)
+        {
+            options.AddDefaultPolicy(ConfigureDefaultCorsPolicy);
+        }
+
+        private static void ConfigureDefaultCorsPolicy(CorsPolicyBuilder builder)
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyMethod();
+            builder.AllowAnyHeader();
         }
     }
 }
