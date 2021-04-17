ecc07f17756e2a96142b10b852613b5f89c18b15
Klayton <odevklayton@gmail.com>
2021-04-17 02:40:21 -0300

    Update Entity

--- src/Restaurant.Core/Entities/Base/Entity.cs
+++ src/Restaurant.Core/Entities/Base/Entity.cs
@@ -7,8 +7,6 @@ namespace Restaurant.Core.Entities.Base
         public Guid Id { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? UpdatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }
-        public bool Deleted => DeletedAt.HasValue;
         public bool Inactivated { get; set; }
 
         public Entity()
